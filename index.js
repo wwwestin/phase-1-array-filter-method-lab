e7cf238e12503d0fbe61eb89de670c2dba50826f
Westin Humble <westin.h@gmail.com>
2022-01-13 18:06:58 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(driverNames, searchName) {
+    const result = driverNames.filter(name => name.toUpperCase() === searchName.toUpperCase())
+
+    return result;
+}
+
+function fuzzyMatch(driverNames, searchName) {
+const namesWithLetter = driverNames.filter ( (name) => name.substring (0,1) === searchName.substring(0,1) )
+
+return namesWithLetter;
+
+}
+
+    
+function matchName(driverNames, searchName) {
+  
+  const match = driverNames.filter ( (name) => name.name === searchName)
+  
+  return match;
+
+}
